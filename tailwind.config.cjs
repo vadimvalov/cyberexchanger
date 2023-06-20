e4cf2d90fd04a745d96ac7b91fbf4ec9407c99d7
aayafrontenda <eeaayeyeah@gmail.com>
2023-06-20 09:56:47 +0600

    config fix, sorting added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // content: ["./node_modules/flowbite/**/*.js"] По документации.
+  content: ["./index.html", "./exchange.html", "./src/**/*.{js,ts,jsx,tsx}"], // content: ["./node_modules/flowbite/**/*.js"] По документации.
   theme: {
     extend: {
       fontFamily: {
