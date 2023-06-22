f2ad30c074c867ee421ce9db7a4cf3bc9739a4a2
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-06-22 17:08:31 +0600

    билд второй страницы

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        exchange: resolve(__dirname, "exchange.html"),
+      },
+    },
+  },
+});
